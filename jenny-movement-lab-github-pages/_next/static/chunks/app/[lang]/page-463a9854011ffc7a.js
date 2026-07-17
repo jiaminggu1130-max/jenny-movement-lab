5e3b7defe8eba7d849a5b9f2c8ba9910c69a7f2b
jiaminggu1130-max <jiaminggu1130@gmail.com>
2026-07-17 08:26:00 +0800

    Add files via upload

---
+++ jenny-movement-lab-github-pages/_next/static/chunks/app/[lang]/page-463a9854011ffc7a.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[495],{2970:function(n,e,u){Promise.resolve().then(u.t.bind(u,7607,23))}},function(n){n.O(0,[607,587,481,744],function(){return n(n.s=2970)}),_N_E=n.O()}]);
